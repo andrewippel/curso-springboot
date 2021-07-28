42cb484e43b4b4f50a1d6539c3b7531a1f868548
Andre Wippel <wippelandre@hotmail.com>
2021-07-27 21:16:56 -0300

    Projeto criado

---
+++ src/test/java/com/andrewippel/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewippel.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
