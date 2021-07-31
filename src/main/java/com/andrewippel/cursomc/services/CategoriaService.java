5532a70f784c59e0d8edbc4f5167f3712b66c229
Andre Wippel <wippelandre@hotmail.com>
2021-07-30 21:52:28 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/andrewippel/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.andrewippel.cursomc.services;
+
+import com.andrewippel.cursomc.domain.Categoria;
+import com.andrewippel.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+
+}
