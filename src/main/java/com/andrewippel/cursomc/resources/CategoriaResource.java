04f847fa7677a0c175e696ac855a7a8aabbd2c72
Andre Wippel <wippelandre@hotmail.com>
2021-08-25 14:23:08 -0300

    listando todas as categorias com dto

--- src/main/java/com/andrewippel/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andrewippel/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.andrewippel.cursomc.resources;
 
 import com.andrewippel.cursomc.domain.Categoria;
+import com.andrewippel.cursomc.dto.CategoriaDTO;
 import com.andrewippel.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -20,6 +23,13 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> find(@PathVariable Integer id) {
         Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
     @RequestMapping(method = RequestMethod.POST)
