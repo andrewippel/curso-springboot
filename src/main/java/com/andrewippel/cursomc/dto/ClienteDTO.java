696450af7c92e62a961380913e056c0417af8abc
Andre Wippel <wippelandre@hotmail.com>
2021-08-26 11:39:34 -0300

    validação customizada para email repetido na atualização de cliente

--- src/main/java/com/andrewippel/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/andrewippel/cursomc/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.andrewippel.cursomc.dto;
 
 import com.andrewippel.cursomc.domain.Cliente;
+import com.andrewippel.cursomc.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
